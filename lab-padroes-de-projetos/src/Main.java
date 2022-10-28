bf819f45b142606b5b8061282e01e031402b4ee6
Abrahao Eneias <abrahao.eneias@dce.ufpb.br>
2022-10-28 18:01:16 -0300

    desafio padroes de projeto

---
+++ lab-padroes-de-projetos/src/Main.java
@@ -0,0 +1,43 @@
+import one.digitalinnovation.gof.facade.Facade;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Singleton
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
+        System.out.println(holder);
+
+        //Strategy
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+
+        robo.setComportamento(agressivo);
+        robo.mover();
+
+        //Facade
+        Facade facade = new Facade();
+        facade.migrarCliente("Abrahao", "5820800");
+
+    }
+
+
+
+}
